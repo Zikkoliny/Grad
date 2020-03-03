1332282e1b8d3f99dba7c8555b4565c4ee0d768d
Сурин Александр Алексеевич <@yxmt.local>
2020-03-03 09:16:10 +0300

    first commit

---
+++ ДемоЭкз/ДемоЭкз/Auth.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ДемоЭкз
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Auth));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.Pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(159, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Login.Location = new System.Drawing.Point(159, 195);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(247, 30);
+            this.Login.TabIndex = 1;
+            // 
+            // Pass
+            // 
+            this.Pass.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Pass.Location = new System.Drawing.Point(159, 278);
+            this.Pass.Name = "Pass";
+            this.Pass.Size = new System.Drawing.Size(247, 30);
+            this.Pass.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(250, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(245, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(74)))), ((int)(((byte)(109)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(229, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(205, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Авторизация";
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pass);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Auth";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Auth_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.TextBox Pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ДемоЭкз/ДемоЭкз/Auth.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+
+namespace ДемоЭкз
+{
+    public partial class Auth : Form
+    {
+        string connectionString;
+        string Log;
+        string Password;
+
+        public Auth()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["ДемоЭкз.Properties.Settings._16is30ConnectionString"].ConnectionString;
+        }
+
+        private void Auth_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Log = Login.Text.Trim();
+            Password = Pass.Text.Trim();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Int32 countUser = 0;
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT COUNT (*) FROM [dbo].[User] WHERE Login = @Login AND Password = @Password";
+                command.Connection = connection;
+
+                command.Parameters.Add("@Login", SqlDbType.VarChar); // НУ ТУТ КАРОЧИ ПЕРЕВОД БУКАФ В СТРИНГ
+                command.Parameters["@Login"].Value = Log;
+
+                command.Parameters.Add("@Password", SqlDbType.VarChar);
+                command.Parameters["@Password"].Value = Password;
+
+                try
+                {
+                    connection.Open();
+                    countUser = (Int32)command.ExecuteScalar();
+                    if (countUser == 1)
+                    {
+                        MessageBox.Show("Авторизация успешна.");
+                    }
+                    else
+                    {
+                        MessageBox.Show(" Ошибка авторизации.");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+    }
+}
